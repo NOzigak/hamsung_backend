a6f714515c2627d265ebc0be2e0f03a1f0099d1f
koreaioi <koreaioi@gachon.ac.kr>
2024-07-03 10:10:58 +0900

    feat: User 오류 수정

--- hamsung_project/src/main/java/hamsung/hamsung_project/entity/User.java
+++ hamsung_project/src/main/java/hamsung/hamsung_project/entity/User.java
@@ -48,8 +48,8 @@ public class User {
     @OneToMany(mappedBy = "user")
     private List<Comment> comments;
 
-    @OneToMany(mappedBy = "user")
-    private List<Board> recruits;
+    @OneToMany(mappedBy = "users")
+    private List<Recruit> recruits;
 
 
 }
