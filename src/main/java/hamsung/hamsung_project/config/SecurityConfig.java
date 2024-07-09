ab09789ef3930d6cb6049d1727110cbfb8d2d3c0
koreaioi <koreaioi@gachon.ac.kr>
2024-07-10 02:16:06 +0900

    feat: CORS 127.0.0.1

--- src/main/java/hamsung/hamsung_project/config/SecurityConfig.java
+++ src/main/java/hamsung/hamsung_project/config/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig {
                         CorsConfiguration configuration = new CorsConfiguration();
 
                         //configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
-                        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://localhost:3000", "https://nozigak.site"));
+                        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://localhost:3000", "https://nozigak.site", "https://127.0.0.1:3000"));
                         
                         configuration.setAllowedMethods(Collections.singletonList("*"));
                         configuration.setAllowCredentials(true);
